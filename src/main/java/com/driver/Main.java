ae38dbc1c6c89448282eedfb1ef1f1ec990de8a2
imsidthakur <106941477+imsidthakur@users.noreply.github.com>
2024-10-01 06:53:18 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -4,7 +4,7 @@ public class Main {
   public static void main(String []args)
   {
     RWOnly obj = new RWOnly();
-    obj.setName = "Siddharth";
+    obj.setName("Siddharth");
     System.out.println(obj.getName());
   }
 }
